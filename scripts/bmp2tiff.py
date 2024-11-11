9a70354099e82e4503c72d245abc8e42d997fd60
owasikohu <owatas2022@gmail.com>
2024-11-11 22:38:14 +0900

    add bmp2jpeg.py

--- scripts/bmp2tiff.py
+++ scripts/bmp2tiff.py
@@ -8,10 +8,10 @@ def convert_bmp_to_tiff(input_bmp_path, output_tiff_path, dpi):
 
         bmp_image.save(output_tiff_path, format='TIFF', dpi=dpi)
 
-# 変換するファイルのパスを指定
-input_bmp = 'main.bmp'  # ここに入力 BMP のパスを指定
-output_tiff = 'print.tiff'  # ここに出力 TIFF のパスを指定
-dpi_value = (600, 600)  # ここに希望の DPI を指定
 
-# 変換実行
+input_bmp = 'main.bmp'
+output_tiff = 'print.tiff'
+dpi_value = (600, 600)
+
+
 convert_bmp_to_tiff(input_bmp, output_tiff, dpi=dpi_value)
